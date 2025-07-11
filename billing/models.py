c12bd8efc73b52aad2708a5dc688a769090a374d
Parakram <parakram@keemail.me>
2025-07-11 21:19:06 +0545

    Refactor billing models and add new entities

--- billing/models.py
+++ billing/models.py
@@ -1,23 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-class Customer(models.Model):
-    sc_no = models.CharField(max_length=20, null=True, blank=True)
+
+# Base Table with no Foreign Keys
+
+class DemandType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    status = models.BooleanField(default=True)
+    rate_per_unit = models.FloatField(null=False, blank=False)
+    minimum_charge = models.FloatField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'demand_types'
+
+
+class Role(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
-    address = models.TextField(null=False, blank=False)
-    dob = models.DateField(null=False, blank=False)
-    contact_no = models.CharField(max_length=20, null=False, blank=False)
-    citizenship_no = models.CharField(max_length=50, unique=True, null=False, blank=False)
-    location_citizenship = models.CharField(max_length=100, null=False, blank=False)
-    property_file_location = models.CharField(max_length=255, null=False, blank=False)
-    branch_id = models.IntegerField(null=False, blank=False)
-    demand_type_id = models.IntegerField(null=False, blank=False)
     status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        db_table = 'customer'
+        db_table = 'roles'
+
+
+class EmployeeType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'employee_types'
 
 
 class PaymentMethod(models.Model):
@@ -32,37 +51,68 @@ class PaymentMethod(models.Model):
         db_table = 'payment_method'
 
 
-class Role(models.Model):
+class Branch(models.Model):
+    branch_code = models.CharField(max_length=20, unique=True, null=False, blank=False)
     name = models.CharField(max_length=100, null=False, blank=False)
+    address = models.TextField(null=False, blank=False)
+    contact = models.CharField(max_length=50, null=False, blank=False)
     status = models.BooleanField(default=True)
+    # this is set to null to prevent circular dependency as branch has branch_admin(emp) and emp has branch so well...
+    branch_incharge = models.ForeignKey(
+        'Employee',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='branches_incharge_of'
+    )
+
+    def __str__(self):
+        return f"{self.branch_code} - {self.name}"
+
+    class Meta:
+        db_table = 'branch'
+        ordering = ['branch_code']
+
+
+class Employee(models.Model):
+    contact_no = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    employee_code = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=200)
+    address = models.TextField(null=False, blank=False)
+    citizenship_no = models.CharField(max_length=50, unique=True)
+    citizenship_file_location = models.CharField(max_length=255, null=True, blank=True)
+    branch = models.ForeignKey(Branch, on_delete=models.PROTECT)
+    employee_type = models.ForeignKey(EmployeeType, on_delete=models.PROTECT)
+    status = models.BooleanField(default=False,help_text="Approved status")  # False until approved
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)  # Linked after approval
+    date_joined = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.employee_code})"
+
+    class Meta:
+        db_table = 'employee'
+        ordering = ['name']
+
+
+class Customer(models.Model):
+    sc_no = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    address = models.TextField(null=False, blank=False)
+    dob = models.DateField(null=False, blank=False)
+    contact_no = models.CharField(max_length=20, null=False, blank=False, unique=True)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    citizenship_no = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    location_citizenship = models.CharField(max_length=100, null=False, blank=False)
+    property_file_location = models.CharField(max_length=255, null=False, blank=False)
+    branch = models.ForeignKey(Branch, on_delete=models.PROTECT,null=False, blank=False)
+    demand_type = models.ForeignKey(DemandType, on_delete=models.PROTECT,null=False, blank=False)
+    created_by = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.BooleanField(default=False,help_text="Approved status")
+
     def __str__(self):
         return self.name
-    class Meta:
-        db_table = 'roles'
-
-class EmployeeType(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    status = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
 
     class Meta:
-        db_table = 'employee_types'
-
-class DemandType(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    status = models.BooleanField(default=True)
-    rate_per_unit = models.FloatField(null=False, blank=False)
-    minimum_charge = models.FloatField(null=False, blank=False)
-    def __str__(self):
-        return self.name
-    class Meta:
-        db_table = 'demand_types'
-#
-# class Branch(models.Model):
-#     branch_code = models.CharField(max_length=20, null=False, blank=False)
-#     name = models.CharField(max_length=100, null=False, blank=False)
-#     address = models.TextField(null=False, blank=False)
-#     contact = models.TextField(null=False, blank=False)
-#     status = models.BooleanField(default=True)
+        db_table = 'customer'
