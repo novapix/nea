e0b2ea64b45f9fac1f2a69c06fd9b4bb43d825d5
Parakram <parakram@keemail.me>
2025-07-15 00:52:27 +0545

    Update settings.py

--- nea/settings.py
+++ nea/settings.py
@@ -22,11 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hv3ms!^%h0%3cw&2m$47q+0ld=rui$aojwx-y4zco*o+_tp_jd'
-
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = []
