eae1ed17ca98715f5c2453129e89b1091e652201
Parakram <parakram@keemail.me>
2025-07-15 15:29:52 +0545

    Add password reset functionality and template

--- nea/urls.py
+++ nea/urls.py
@@ -17,9 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import  settings
-from billing.views import superadmin_dashboard
+from billing.views import superadmin_dashboard, login_view, logout_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', login_view, name='login'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('forgot-password/', auth_views.PasswordResetView.as_view(
+        template_name='auth/password_reset.html'
+    ), name='password_reset'),
+ 
+    path('forgot-password/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='auth/password_reset_done.html'
+    ), name='password_reset_done'),
+ 
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='auth/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+ 
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='auth/password_reset_complete.html'
+    ), name='password_reset_complete'),
     # path('admin/', admin.site.urls),
     path('admin/', superadmin_dashboard, name='superadmin_dashboard'),
 ]
