6ccaf006f5c7341352efc6b1ad8eacf76bd6ac46
Parakram <parakram@keemail.me>
2025-07-19 10:11:06 +0545

    Remove Depreciated UserModel

--- billing/admin.py
+++ billing/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import (
     DemandType, Role, EmployeeType, PaymentMethod, Branch, Employee,
-    Customer, UserProfile, NepaliMonth
+    Customer, NepaliMonth
 )
 
 admin.site.register(DemandType)
@@ -11,5 +11,4 @@ admin.site.register(PaymentMethod)
 admin.site.register(Branch)
 admin.site.register(Employee)
 admin.site.register(Customer)
-admin.site.register(UserProfile)
 admin.site.register(NepaliMonth)
